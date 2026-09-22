f3ef73175c5a1d12e865b970a732cf28c57d9f34
modjuju <edenqtminari@gmail.com>
2026-09-23 01:31:47 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "being silly and full of whimsy hihi ૮꒰˶ᵔ ᵕ ᵔ˶꒱ა",     // this is what people actually see
+        state: "silly bnuuy ૮꒰˶ᵔ ᵕ ᵔ˶꒱ა",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
